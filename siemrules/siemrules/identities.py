e19cd6f439d85dd7bb3a2469223ce94d8e8f5893
Fadl <chaos@efqr.dev>
2025-04-29 14:44:20 +0100

    add summary and description to retrieve object #69

--- siemrules/siemrules/identities.py
+++ siemrules/siemrules/identities.py
@@ -29,7 +29,19 @@ from drf_spectacular.utils import extend_schema, extend_schema_view
     ),
     list=extend_schema(
         summary="Search identity objects",
-        description="",
+        description=textwrap.dedent(
+            """
+            This endpoint will allow you to search for all identities that exist.
+            """
+        ),
+    ),
+    retrieve=extend_schema(
+        summary="GET identity object by STIX ID",
+        description=textwrap.dedent(
+            """
+            This endpoint will allow you to GET an identity object by its STIX ID.
+            """
+        ),
     ),
 )
 class IdentityView(viewsets.ViewSet):
