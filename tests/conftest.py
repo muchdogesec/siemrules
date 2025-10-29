f0e890a68c85cc0b93b9f860579e13aaddaee758
Fadl <chaos@efqr.dev>
2025-10-29 15:21:38 +0100

    fix st issue

---
+++ tests/conftest.py
@@ -0,0 +1,8 @@
+
+@pytest.fixture(autouse=True)
+def default_profile(db):
+    return Profile.objects.create(
+        id=uuid.UUID('5e2c00bc-4e83-48b0-83dd-3fa084322245'),
+        name='default-profile',
+        is_default=True,
+    )
